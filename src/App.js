9eb658d18f234ab23507e9cef94afc5a28033a98
Juan Galarce <j6alarce@gmail.com>
2021-10-21 11:47:20 -0300

    Se crea un componente input con sus estilos para los campos del formulario

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from "react";
 import styled from "styled-components";
+import { Formik, Form } from "formik";
+import Input from "./components/Input";
 
 // Centrar Contenido :v
 const Container = styled.div`
@@ -7,12 +9,40 @@ const Container = styled.div`
   justify-content: center;
   height: 100%;
   align-items: center;
+  background-color: #333;
+`;
+
+const Section = styled.section`
+  background-color: #646464;
+  border-top: solid 2px palevioletred;
+  padding: 20px 25px;
+  width: 600px;
+  box-shadow: 0px 2px 3px rgb(0, 0, 0, 0.3);
 `;
 
 const App = () => {
+  const handleSubmit = () => {};
+
   return (
     <Container>
-      <h1>Hello world</h1>
+      <Section>
+        <Formik
+          initialValues={{
+            deposit: "",
+            contribution: "",
+            years: "",
+            rate: "",
+          }}
+          onSubmit={handleSubmit}
+        >
+          <Form>
+            <Input name='deposit' label='Depósito inicial' />
+            <Input name='contribution' label='Contribución Anual' />
+            <Input name='years' label='Años' />
+            <Input name='rate' label='Interes estimado' />
+          </Form>
+        </Formik>
+      </Section>
     </Container>
   );
 };
---
+++ src/components/Input.js
@@ -0,0 +1,42 @@
+import React from "react";
+import styled from "styled-components";
+import { useField } from "formik";
+
+const Control = styled.div`
+  margin-bottom: 20px;
+`;
+
+const Label = styled.label`
+  color: #fff;
+  display: block;
+  margin-bottom: 5px;
+`;
+
+const MyInput = styled.input`
+  outline: none;
+  padding: 8px;
+  border: solid 1px #b1b3b5;
+  border-radius: 4px;
+  width: 100%;
+  margin-bottom: 5px;
+`;
+
+const ErrorMessage = styled.div`
+  color: red;
+`;
+
+const Input = ({ label, ...props }) => {
+  const [field, meta] = useField(props);
+
+  return (
+    <Control>
+      <Label>{label}</Label>
+      <MyInput {...field} {...props} />
+      {meta.touched && meta.error ? (
+        <ErrorMessage>{meta.error}</ErrorMessage>
+      ) : null}
+    </Control>
+  );
+};
+
+export default Input;
